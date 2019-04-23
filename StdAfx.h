2393d00bebd8ea1fd2579d3f6600b4eea76e3741
h54188 <49894911+h54188@users.noreply.github.com>
2019-04-23 20:50:40 +0800

    20190423
    this is a program of login test and a guess number game.

---
+++ StdAfx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__9DE0D4B4_1C77_47FE_9350_AD9C7F043D26__INCLUDED_)
+#define AFX_STDAFX_H__9DE0D4B4_1C77_47FE_9350_AD9C7F043D26__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__9DE0D4B4_1C77_47FE_9350_AD9C7F043D26__INCLUDED_)
